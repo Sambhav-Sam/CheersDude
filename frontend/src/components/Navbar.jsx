7828426dbc654f5624621f29497ba3c84602308a
Rupesh sharma <rsharma@Rk-Air.local>
2024-06-20 22:55:49 +0530

    add navbar2

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
                 <button className="btn btn-ghost btn-circle hidden md:flex">
       <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
     </button>
-                    {/* {cart items} */}
+                    {/* cart items */}
                     <div className=" hidden lg:flex items-center justify-center px-3">
                         <div className="dropdown dropdown-end">
                             <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
